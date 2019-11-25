415310b8246701618538ab6e0a0a468c47111007
Nikita Grishko <gr1n@protonmail.com>
2019-11-25 17:57:55 +0300

    chore: format zsh files

--- direnv/direnv.plugin.zsh
+++ direnv/direnv.plugin.zsh
@@ -1,8 +1,8 @@
 _direnv_hook() {
-    eval "$("/usr/bin/env" direnv export zsh)";
+  eval "$("/usr/bin/env" direnv export zsh)";
 }
 
 typeset -ag precmd_functions;
 if [[ -z ${precmd_functions[(r)_direnv_hook]} ]]; then
-    precmd_functions+=_direnv_hook;
+  precmd_functions+=_direnv_hook;
 fi
